8e890cf88ddfbf2654277e87d4293b202560d0bf
Greg Moua <gregmoua@gmail.com>
2022-01-31 23:38:02 -0800

    -Fires now grow with ticks and spawn in the right areas -Implemented more logic for endgame       -Landing helicopter and checking all fires -Improved readability by reducing all wide lines to <80 chars -Drew helicopter line to indicate direction
    -Currently working on helicopter movement

--- common/src/main/java/org/csc133/a1/AppMain.java
+++ common/src/main/java/org/csc133/a1/AppMain.java
@@ -14,6 +14,7 @@ import com.codename1.ui.util.UITimer;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -89,8 +90,8 @@ class GameWorld {
         helipad = new Helipad();
         river = new River();
         fires = new ArrayList<>();
-        for(int i = 0; i < NUMBER_OF_FIRES;i++) {
-            fires.add(new Fire());
+        for(int i = 1; i <= NUMBER_OF_FIRES;i++) {
+            fires.add(new Fire(i));
         }
     }
 
@@ -133,9 +134,58 @@ class GameWorld {
         for (Fire fire : fires) {
             fire.draw(g);
         }
+        tick();
     }
 
     public void tick() {
+
+        //if all fires out and speed is 0
+        //and resting on helipad you win
+        if(allFiresOut(fires) && helicopter.getSpeed() == 0 && landCopter()){
+            //victory condition
+            //ask to play again or quit
+
+        //if you run out of fuel you lose
+        } else if (helicopter.getFuel() <= 0) {
+            //defeat
+            //ask to play again or quit
+        } else {
+            //else grow the fires by random amount 1-10
+            //and continue the game
+            for(Fire fire : fires) {
+                fire.grow();
+            }
+        }
+
+        //move forward
+    }
+
+    public boolean landCopter(){
+        Point copter = helicopter.getLocation();
+        Point pad = helipad.getLocation();
+        int pWidth = Display.getInstance().getDisplayWidth()/10;
+        int pLength = Display.getInstance().getDisplayWidth()/10;
+
+        //this conditional makes sure the location of the helicopter is
+        //within the helipad
+        //if copter.getX is > pad.getX but less than pad.getX + its width
+        //and copter.getY is > pad.getY but less than pad.getY + its length
+        if((copter.getX() > pad.getX()
+            && copter.getX() < (pad.getX() + pWidth))
+            && copter.getY() > pad.getY()
+            && copter.getY() < pad.getY() + pLength){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean allFiresOut(ArrayList<Fire> fires) {
+        for(Fire fire: fires) {
+            if(fire.getSize() > 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
 
@@ -150,7 +200,8 @@ class River {
     }
 
     private void init() {
-        location = new Point(0, Display.getInstance().getDisplayHeight() / 3);
+        location = new Point(0,
+                             Display.getInstance().getDisplayHeight() / 3);
     }
     public void draw(Graphics g) {
 
@@ -177,8 +228,9 @@ class Helipad {
 
         //set the location of the helipad equal
         //to middle of screen at the bottom (9/10 of the way down)
-        location = new Point(Display.getInstance().getDisplayWidth() / 2,Display.getInstance().getDisplayHeight() -
-                Display.getInstance().getDisplayHeight() / 10);
+        location = new Point(Display.getInstance().getDisplayWidth() / 2,
+                             Display.getInstance().getDisplayHeight() -
+                                Display.getInstance().getDisplayHeight() / 10);
         }
     public void draw(Graphics g) {
 
@@ -196,21 +248,67 @@ class Helipad {
         g.drawArc(location.getX()- offset,location.getY(),
                     width, length, 0, 360);
     }
+
+    //need this specific getter to make sure
+    //the helicopter and helipad spawn with each other
+    public Point getLocation(){
+        return location;
+    }
 }
 
 class Fire {
     private Point location;
     private int size;
+    int number;
+    //using a number parameter to number each fire
+    //to know where draw it
+    public Fire(int number){
 
-    public Fire(){
+        this.number = number;
+        int width = Display.getInstance().getDisplayWidth();
+        int height = Display.getInstance().getDisplayHeight();
 
-        size = new Random().nextInt(100);
+        //creating conditional statements to draw the fires in random
+        //but sectioned coordinates
+        //Fire 1 will always be in the upper left
+        //Fire 2 will always be in the upper right
+        //Fire 3 will always be in the lower bottom
+        if(number == 1) {
+            location = new Point(
+                    new Random().nextInt(width / 10) + width / 10,
+                    new Random().nextInt(height / 10) + height /10);
+            size = new Random().nextInt(width / 6)+50;
+
+        } else if (number == 2) {
+            location = new Point(
+                    new Random().nextInt(width / 10) + (6 * width / 10),
+                    new Random().nextInt(height / 10) + height / 10);
+            size = new Random().nextInt(width / 5)+75;
+
+        } else if (number == 3) {
+            location = new Point(
+                    new Random().nextInt(width / 2) + width / 10,
+                    new Random().nextInt(height / 10) + (7 * height / 10));
+            size = new Random().nextInt(width / 4)+100;
+
+        } else {
+            size = new Random().nextInt(500)+50;
+            location = new Point(new Random().nextInt(500),
+                                 new Random().nextInt(500));
+        }
     }
 
     public void draw(Graphics g) {
-
         g.setColor(ColorUtil.MAGENTA);
-        g.fillArc(new Random().nextInt(500), new Random().nextInt(500), size, size, 0, 360);
+        g.fillArc(location.getX(), location.getY(), size, size, 0, 360);
+    }
+
+    public void grow(){
+        size += new Random().nextInt(3);
+    }
+
+    public int getSize(){
+        return size;
     }
 
 }
@@ -223,22 +321,43 @@ class Helicopter {
     private static int maxSpeed;
     private static int maxWater;
     private int heading;
+    private Helipad helipad;
 
     public Helicopter(){
         init();
     }
 
     private void init(){
+        int height = Display.getInstance().getDisplayHeight() / 50;
+        int width = Display.getInstance().getDisplayWidth() / 30;
         fuel = 25000;
         maxSpeed = 10;
         maxWater = 1000;
         speed = 0;
         water = 0;
+        helipad = new Helipad();
+        heading = 0;
+        location = new Point(
+                helipad.getLocation().getX() - width,
+                helipad.getLocation().getY() + height);
     }
 
+    public void setHeading(int heading){
+        this.heading = heading;
+    }
     public Point getLocation() {
         return location;
     }
+
+    public void setLocation(Point location) {
+        this.location = location;
+    }
+
+    public void setFuel(int fuel) { this.fuel = fuel;}
+
+    public int getFuel() { return fuel;}
+
+    public int getSpeed() {return speed;}
 
     public void drinkWater(){
         if (speed <= 2 && water < 1000) {
@@ -260,16 +379,25 @@ class Helicopter {
         }
     }
 
+    public void fight(){
 
+    }
 
     public void draw(Graphics g) {
         g.setColor(ColorUtil.YELLOW);
-        g.fillArc(Display.getInstance().getDisplayWidth()/2,
-                Display.getInstance().getDisplayHeight()/2,
-                50,50,0,360);
+
+        //drawing a filled circle and line relative to its location
+        g.fillArc(location.getX(), location.getY(),50,50,0,360);
+        g.drawLine(location.getX()+ location.getX()/25,
+                   location.getY()+location.getY()/50,
+                   location.getX()+ location.getX()/25,
+                   location.getY()- location.getY()/30);
     }
 
     public void walk(){
+        fuel = fuel - ((speed * speed) + 5);
+
+        //based on direction pointed it will determine where we steer
 
     }
 
